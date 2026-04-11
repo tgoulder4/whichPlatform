3aacb4c1c273943deb5ab90a8431d1a04a83b6f6
goulde_t <tye.goulder@warwick.ac.uk>
2026-04-11 15:33:33 +0100

    accessible travel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,9 @@ function Page({ }: Props) {
 
       <div className="flex flex-col items-center bg-white p-4">
 
-        <h1 className='font-bold text-3xl'>Get the First Class Treatment You Deserve</h1>
+        {/* <h1 className='font-bold text-3xl'>Get the First Class Treatment You Deserve</h1> */}
+        <h1 className='font-bold text-3xl'>Accessible Travel for Everyone</h1>
+
         <h2 className='text-2xl font-light'>Be the first to board your train departing from London Euston</h2>
         <Button onClick={() => Cookie.set('IntroPassed', 'T')} asChild className='mt-4'>
 
